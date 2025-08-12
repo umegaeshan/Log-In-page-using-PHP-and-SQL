0002d136f241fe9e726e7a2d7ab1b0ffa40a414d
unknown <umegaeshan@gmail.com>
2025-08-12 15:26:36 +0530

    done sign -up form html css part and create connevtion file

---
+++ sign-up-form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sign Up Form</title>
+	<link rel="stylesheet" type="text/css" href="sign-up-form.css">
+</head>
+<body>
+
+
+<div class="container">
+
+	<div class="form">
+		
+		<h1> Signup Form</h1>
+
+			<p class="head_para"> it's quick and easy </p>
+
+			<div class="input_boxes">
+
+				<input type="text" name="fullname" placeholder="Full Name "><br>
+
+				<input type="email" name="email" placeholder="Email Address "><br>
+
+				<input type="password" name="password" placeholder="Password "><br>
+
+				<input type="password" name="confirm_passsword" placeholder="Confirm Password "><br>
+
+			</div>
+
+			<div class="btn">
+				<input type="submit" name="signup" value="Sign Up">
+			</div>
+
+			<div class="footer_para">
+				Already a member ? 
+				<a href=""> Login Here </a>
+			</div>
+
+	</div>
+</div>
+
+
+</body>
+</html>
